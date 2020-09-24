a03034fc769a3756e2eec697392e1d4af906812e
avnish986 <avnishgupta986@gmail.com>
2020-09-24 13:09:54 +0530

    [Avnish]Added:UC2 push

--- src/com/get_wage/EmpWageCalc.java
+++ src/com/get_wage/EmpWageCalc.java
@@ -4,11 +4,17 @@ public class EmpWageCalc {
 
 	public static void main(String[] args) {
 		int Is_Full_Time=1;
+		int Emp_Rate_Per_hour=20;
+		int empHrs=0;
+		int empWage=0;
+		for(int i=0;i<10;i++) {
 		double empCheck=Math.floor(Math.random()*10)%2;
 		if(empCheck==Is_Full_Time)
-		System.out.println("Employee Present");
+		empHrs=8;
 		else
-		System.out.println("Employee Absent");
+		empHrs=0;
+		empWage= empHrs * Emp_Rate_Per_hour;
+		System.out.println("Emp Wage : "+empWage);
 	}
 
 }
