479cc87e3e1d11049f3260c42538f50e32001e18
avnish986 <avnishgupta986@gmail.com>
2020-09-28 14:10:27 +0530

    [Avnish]Added: uc14 replying wage query when queried by company

--- src/com/get_wage/EmpWageCalc.java
+++ src/com/get_wage/EmpWageCalc.java
@@ -9,33 +9,33 @@ public class EmpWageCalc implements ComputeEmpWageInterface{
 	public static final int IS_FULL_TIME = 1;
 	public static final int IS_PART_TIME = 2;
    
-	private ArrayList<EmpWageComp> EmpWageCompList;
+	private ArrayList<EmpWageComp> companyEmpWageList;
 	private Map<String, EmpWageComp> companyToEmpWageMap;
 	
     public EmpWageCalc() {
-    	EmpWageCompList = new ArrayList<>();
+    	companyEmpWageList = new ArrayList<>();
     	companyToEmpWageMap = new HashMap<>();
     }
     @Override
     public void addCompanyEmpWage(String company, int empRatePerHour,int numOfWorkingDays, int maxHoursPerMonth ) {
-		EmpWageComp EmpWageComp = new EmpWageComp(company, empRatePerHour, numOfWorkingDays, maxHoursPerMonth);
-		EmpWageCompList.add(EmpWageComp);
-		companyToEmpWageMap.put(company, EmpWageComp);
+    	EmpWageComp companyEmpWage = new EmpWageComp(company, empRatePerHour, numOfWorkingDays, maxHoursPerMonth);
+		companyEmpWageList.add(companyEmpWage);
+		companyToEmpWageMap.put(company, companyEmpWage);
 
     }
     @Override
 	public void computeWage() {
-    	for(int i = 0; i < EmpWageCompList.size(); i++){
-		EmpWageComp EmpWageComp = EmpWageCompList.get(i);
-		EmpWageComp.setTotalEmpWage(this.computeEmpWage(EmpWageComp));
-		System.out.println(EmpWageComp);
+    	for(int i = 0; i < companyEmpWageList.size(); i++){
+    		EmpWageComp companyEmpWage = companyEmpWageList.get(i);
+		companyEmpWage.setTotalEmpWage(this.computeWage(companyEmpWage));
+		System.out.println(companyEmpWage);
       }
 	}
-	private int computeEmpWage(EmpWageComp EmpWageComp) {
+	private int computeWage(EmpWageComp companyEmpWage) {
 		int empHours = 0, totalEmpHours = 0, totalWorkingDays = 0;
 
 
-		while (totalEmpHours <= EmpWageComp.maxHoursPerMonth && totalWorkingDays < EmpWageComp.numOfWorkingDays) {
+		while (totalEmpHours <= companyEmpWage.maxHoursPerMonth && totalWorkingDays < companyEmpWage.numOfWorkingDays) {
 			totalWorkingDays++;
             int empCheck = (int)Math.floor(Math.random()* 10)% 3;
 
@@ -54,19 +54,20 @@ public class EmpWageCalc implements ComputeEmpWageInterface{
 			totalEmpHours += empHours;
 			System.out.println("Day: " + totalWorkingDays + " Emp Hr:  " + empHours);
 		}
-       return totalEmpHours * EmpWageComp.empRatePerHour;
+       return totalEmpHours * companyEmpWage.empRatePerHour;
 
 	}
 	
 	@Override
 	public int getTotalWage(String company) {
-		return 0;
+		return companyToEmpWageMap.get(company).totalEmpWage;
 	}
 	 public static void main(String[] args) {
 		 EmpWageCalc empWageBuilder = new EmpWageCalc();
 		 empWageBuilder.addCompanyEmpWage("Reliance",  20,  5,  10);
 		 empWageBuilder.addCompanyEmpWage("BigBasket",  10,  4,  20);
 		 empWageBuilder.computeWage();
+		 System.out.println("Total Wage for BigBasket: "+ empWageBuilder.getTotalWage( "BigBasket"));
 	 }
 	
 }
