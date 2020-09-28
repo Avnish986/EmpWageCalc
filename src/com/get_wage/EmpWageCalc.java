3def90e8c091c952fbf07decc63d39de7c06f9e4
avnish986 <avnishgupta986@gmail.com>
2020-09-28 13:03:38 +0530

    [Avnish]Refactored:uc7 using class method

--- src/com/get_wage/EmpWageCalc.java
+++ src/com/get_wage/EmpWageCalc.java
@@ -2,36 +2,56 @@ package com.get_wage;
 
 public class EmpWageCalc {
 
-	public static final int Is_Part_Time=1;
-	public static final int Is_Full_Time=2;
-	public static final int Emp_Rate_Per_Hour=20;
-	public static final int Num_Of_Working_Days=2;
-	public static final int Max_Hrs_Per_Month=10;
-	public static void main(String[] args) {
-	int empHrs=0, totalEmpHrs=0, totalWorkingDays=0;
-
-	while(totalEmpHrs<=Max_Hrs_Per_Month && totalWorkingDays<Num_Of_Working_Days)
+	public static final int isPartTime=1;
+	public static final int isFullTime=2;
+	public static final int empRatePerHr=20;
+	public static final int numOfDays=20;
+	public static final int maxHrPerMonth=100;
+	public String attendance()
 	{
-	totalWorkingDays++;
-	int empCheck=(int)Math.floor(Math.random()*10)%3;
-	switch(empCheck) {
-	case Is_Part_Time:
-	empHrs=4;
-	break;
-
-	case Is_Full_Time:
-	empHrs=8;
-	break;
-
-	default:
-	empHrs=0;
+		double empCheck=Math.floor(Math.random()*10)%2;
+		if(empCheck==isFullTime)
+			return "Employee Present";
+		else
+			return "Employee Absent";
 	}
-	totalEmpHrs+=empHrs;
-	System.out.println("Day#: "+totalWorkingDays+" EmpHrs: "+empHrs);
+	public double empFullPartStatus()
+	{
+		double empCheck=Math.floor(Math.random()*10)%3;
+		return empCheck;
 	}
-	int totalEmpWage=totalEmpHrs*Emp_Rate_Per_Hour;
-	System.out.println("Total Emp Wage : "+totalEmpWage);
+	public int totalWage()
+	{
+		int empHrs=0, totalEmpHrs=0, totalWorkingDays=0;
+		while(totalEmpHrs<=maxHrPerMonth && totalWorkingDays<numOfDays)
+		{
+		totalWorkingDays++;
+		int fullPartStatus=(int) empFullPartStatus();
+		switch(fullPartStatus) {
+		case isPartTime:
+		empHrs=4;
+		break;
 
+		case isFullTime:
+		empHrs=8;
+		break;
+
+		default:
+		empHrs=0;
+		}
+		totalEmpHrs+=empHrs;
+		System.out.println("Day#:"+totalWorkingDays+" EmpHrs: "+empHrs);
+		}
+		int totalEmpWage=totalEmpHrs*empRatePerHr;
+		return totalEmpWage;
+	}
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Wage Computation");
+		EmpWageCalc empobj = new EmpWageCalc();
+		//String status = empobj.attendance();
+		//System.out.println(status);
+		int totalWage = empobj.totalWage();
+		System.out.println("Emp Total Wage : "+totalWage);
 	}
 
 }
